a9c9c8fe7bdd14f8c89bf5ead3cdebca9edea5a2
vitor barbosa <vitorbarbosa789@gmail.com>
2024-10-05 20:15:20 -0300

    [ADD] END

--- src/components/donation/DonationComplete.jsx
+++ src/components/donation/DonationComplete.jsx
@@ -1,4 +1,7 @@
-import {donationMotivations, donationForms} from '../../data/donationData'
+import {donationMessage, donationMotivations, donationTypes, acknowledgmentDonations} from '../../data/donationData'
+import FormInput from '../reusable/FormInput';
+import Button from '../reusable/Button';
+import Card from '../reusable/Card'
 
 const DonationComplete = () => {
 
@@ -32,18 +35,93 @@ const DonationComplete = () => {
                 <p className="font-general-medium text-2xl sm:text-6xl md:text-7xl lg:text-8xl mb-1 text-ternary-dark dark:text-ternary-light">
                   Formas de Doar
                 </p>
+                <p className="mb-4 text-ternary-dark dark:text-ternary-light text-justify">{donationMessage}</p>
+                <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mt-6 sm:gap-10'>
+                {donationTypes.map((partner) => (
+                    <Card
+                        key={partner.id} 
+                        image={partner.img}
+                        hasButton={true}
+                        textButton={partner.title}
+                    />
+                    ))}
+                </div>
               </div>
-      
-              {donationForms.map((bio) => (
+                 
+            </div>
+          </div>
+
+          <div className="w-full lg:w-1/2">
+			<div className="leading-loose">
+				<form
+					onSubmit={(e) => {
+						e.preventDefault();
+					}}
+					className="max-w-xl m-4 p-6 sm:p-10 bg-secondary-light dark:bg-secondary-dark rounded-xl shadow-xl text-left"
+				>
+					<FormInput
+						inputLabel=""
+						labelFor="name"
+						inputType="text"
+						inputId="name"
+						inputName="name"
+						placeholderText="Nome"
+						ariaLabelName="Name"
+					/>
+          			<FormInput
+						inputLabel=""
+						labelFor="phone"
+						inputType="text"
+						inputId="phone"
+						inputName="phone"
+						placeholderText="Telefone"
+						ariaLabelName="Telefone"
+					/>
+					<FormInput
+						inputLabel=""
+						labelFor="email"
+						inputType="email"
+						inputId="email"
+						inputName="email"
+						placeholderText="Digite seu email"
+						ariaLabelName="Email"
+					/>
+					<FormInput
+						inputLabel=""
+						labelFor="subject"
+						inputType="text"
+						inputId="subject"
+						inputName="subject"
+						placeholderText="Projeto Desejado"
+						ariaLabelName="Subject"
+					/>
+
+          
+					
+					<div className="font-general-medium w-40 px-4 py-2.5 text-white text-center font-medium tracking-wider bg-indigo-500 hover:bg-indigo-600 focus:ring-1 focus:ring-indigo-900 rounded-lg mt-6 duration-500">
+						<Button
+							title="Quero doar"
+							type="submit"
+							aria-label="Quero doar"
+						/>
+					</div>
+				</form>
+			</div>
+		</div>
+		<div className="py-5 sm:py-10 mt-5 sm:mt-10">
+              <p className="text-center  font-general-medium text-4xl sm:text-6xl md:text-7xl lg:text-8xl mb-1 text-ternary-dark dark:text-ternary-light">
+                Reconhecimento
+              </p>
+			  {acknowledgmentDonations.map((bio) => (
                 <p
                   className="mb-4 text-ternary-dark dark:text-ternary-light text-justify"
                   key={bio.id}
                 >
-                  {bio.bio}
+                  {bio.text}
                 </p>
               ))}
-            </div>
-          </div>
+			  <p>Junte-se a nós na construção de um futuro melhor e mais inclusivo!</p>
+        </div>
         </section>
       );
       
--- src/data/donationData.js
+++ src/data/donationData.js
@@ -1,3 +1,8 @@
+import pobre from '../images/pobre.jpg';
+import pouco from '../images/pouco.webp';
+import muito from '../images/mt.webp';
+import medio from '../images/medio.jpg';
+
 export const donationMotivations = [
 	{
 		id: 1,
@@ -49,91 +54,51 @@ export const donationMotivations = [
 	},
 ]
 
-export const donationForms = [
+export const donationMessage = 'Oferecemos várias opções de doação para se adequar às suas preferências:'
+
+export const donationTypes = [
 	{
 		id: 1,
-		bio: (
+		title: 'Doação Única',
+		img: pouco
+	},
+	{
+		id: 2,
+		title: 'Doação Mensal',
+		img: medio
+	},
+	{
+		id: 3,
+		title: 'Doação Corporativa',
+		img: muito
+	}
+]
+
+
+export const acknowledgmentDonations = [
+	{
+		id: 1,
+		text: (
 			<>
-				Oferecemos várias opções de doação para se adequar às suas preferências:
+			Agradecemos a todos os nossos doadores pelo apoio contínuo! Aqui estão algumas histórias de impacto que mostram como suas doações estão fazendo a diferença:
 			</>
 		)
 	},
-	// {
-	// 	id: 2,
-	// 	bio: (
-	// 		<>
-	// 			<li>
-	// 				Ana - Diretora Executiva <br/> Com mais de 15 anos de experiência em gestão de ONGs e desenvolvimento social, Ana lidera nossa organização com 
-	// 				uma visão focada na inovação e no impacto comunitário. Ela é responsável por traçar a estratégia geral da fundação e garantir a implementação 
-	// 				de políticas que promovam nosso crescimento sustentável. Sua liderança é reconhecida por sua abordagem colaborativa e orientada a 
-	// 				resultados.
-	// 			</li>
-	// 		</>
-	// 	)
-	// },
-	// {
-	// 	id: 3,
-	// 	bio: (
-	// 		<>
-	// 			<li>
-	// 				Carlos Oliveira - Líder de Projetos<br/> Especialista em planejamento de projetos e tecnologia educacional, Carlos coordena as 
-	// 				principais iniciativas da fundação, garantindo que os projetos sejam executados de acordo com nossos valores e metas. 
-	// 				Com uma carreira de mais de 10 anos na área de gestão de projetos, ele é peça-chave na execução das nossas 
-	// 				atividades voltadas ao desenvolvimento social e educacional.
-	// 			</li>
-	// 		</>
-	// 	)
-	// },
-	// {
-	// 	id: 4,
-	// 	bio: (
-	// 		<>
-	// 			<li>
-	// 				Maria Fernanda Lima - Gerente Financeira <br/>
-	// 				Maria Fernanda é uma especialista em finanças com mais de 12 anos de experiência em auditoria e controle financeiro no terceiro setor. 
-	// 				Ela é responsável por manter a integridade financeira da nossa organização, assegurando que os recursos sejam utilizados de 
-	// 				forma eficaz e transparente.
-	// 			</li>
-	// 		</>
-	// 	)
-	// },
-	// {
-	// 	id: 5, 
-	// 	bio: (
-	// 		<>
-	// 			<li>
-	// 				Conselho Consultivo <br/> 
-	// 				Nosso Conselho Consultivo é formado por especialistas de renome que fornecem insights 
-	// 				estratégicos para apoiar nossa missão e garantir que as melhores práticas sejam seguidas. 
-	// 				Eles trabalham lado a lado com a diretoria para fortalecer nossa governança e promover a 
-	// 				transparência em todas as nossas ações.
-	// 			</li>
-	// 		</>
-	// 	)
-	// },
-	// {
-	// 	id: 6,
-	// 	bio: (
-	// 		<>
-	// 		<li>
-	// 			João Ribeiro - Especialista em Sustentabilidade e Impacto Social <br/>
-	// 			Com 20 anos de experiência em sustentabilidade, João oferece conselhos valiosos sobre como podemos 
-	// 			maximizar nosso impacto social de maneira ética e sustentável.
-	// 		</li>
-	// 		</>
-	// 	)
-	// },
-	// {
-	// 	id: 7,
-	// 	bio: (
-	// 		<>
-	// 		<li>
-	// 			Beatriz Martins - Consultora Jurídica <br/>
-	// 			Advogada com especialização em direito do terceiro setor, Beatriz garante que nossas operações estejam em 
-	// 			conformidade com todas as leis e regulamentos aplicáveis.
-	// 		</li>
-	// 		</>
-	// 	)
-	// }
+	{
+		id: 2,
+		text: (
+			<>
+			<li>João Silva: Recebeu uma bolsa de estudos através da Fundação COMP+, e hoje ele é um desenvolvedor de software bem-sucedido. Sua doação ajudou a financiar sua educação e ele agora está retribuindo à comunidade como mentor.</li>
+			</>
+		)
+	},
+	{
+		id: 3,
+		text: (
+			<>
+			<li>Empresa XYZ: A doação da Empresa XYZ possibilitou a realização de um workshop de inclusão digital para 100 jovens, transformando vidas e capacitando novos talentos.</li>
+			</>
+		)
+	}
 ]
-  
+
